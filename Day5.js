24b35baaacd52c9aacb72fbbe9fc21e82b0179c6
Jaswant K <113849693+JASWANT2001@users.noreply.github.com>
2023-09-26 20:59:14 +0530

    Add files via upload

---
+++ Day5.js
@@ -0,0 +1,15 @@
+
+    "basics": {
+      "name": "JASWANT K",
+    
+      
+      "email": "k.jaswant2305@gmail.com",
+      "phone": "9585221288",
+      "degree": "B.E in Electronics and Communication",
+      
+      "summary": "I'm a fast learner ",
+
+      "location": {
+        "address": "5B/1 Subbaya Layout",
+        "postalCode": "641045",
+        "city": "Coimbatore"} }
