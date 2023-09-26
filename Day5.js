8ad360b911041b7515c66535eaf56566dfc80100
Jaswant K <113849693+JASWANT2001@users.noreply.github.com>
2023-09-26 21:15:23 +0530

    Day5.js

--- Day5.js
+++ Day5.js
@@ -1,14 +1,16 @@
-    "basics": {
+// Own resume data using JSON Format:
+
+
+"basics": {
       "name": "JASWANT K",
-    
-      
       "email": "k.jaswant2305@gmail.com",
       "phone": "9585221288",
       "degree": "B.E in Electronics and Communication",
-      
       "summary": "I'm a fast learner ",
 
       "location": {
         "address": "5B/1 Subbaya Layout",
         "postalCode": "641045",
         "city": "Coimbatore"} } 
+
+//............................................................................................
